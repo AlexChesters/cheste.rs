a9ffd85c8709f2124f6e68702327abbb492a9230
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-04-10 21:00:50 +0100

    add second entry

--- static/src/router.jsx
+++ static/src/router.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './styles/index.scss'
 
 import Blog001 from './routes/blog/001'
+import Blog002 from './routes/blog/002'
 
 export default function router () {
   return (
     <Router>
       <Switch>
         <Route path='/blog/001' component={Blog001} />
+        <Route path='/blog/002' component={Blog002} />
       </Switch>
     </Router>
   )
---
+++ static/src/routes/blog/002.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import BlogEntry from '../../components/BlogEntry'
+
+import Post from '../../../blog/002.md'
+
+export default () => {
+  return <BlogEntry entry={Post} />
+}

cc253591a5a039afc04cbc532982936071394221
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-04-11 18:05:47 +0100

    update mobile styling

--- static/src/router.jsx
+++ static/src/router.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './styles/index.scss'
 
 import Home from './routes/home'
+import BlogPreview from './routes/blog-preview'
 import BlogEntry from './routes/blog-entry'
 
 export default function router () {
@@ -11,6 +12,7 @@ export default function router () {
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/blog' component={BlogPreview} />
         <Route path='/blog/:entry' component={BlogEntry} />
       </Switch>
     </Router>
---
+++ static/src/routes/blog-preview/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import PageSkeleton from '../../components/page-skeleton'
+
+export default function BlogPreview () {
+  return (
+    <PageSkeleton>
+      <section className='blog-preview'>
+        <h1>jasda</h1>
+      </section>
+    </PageSkeleton>
+  )
+}

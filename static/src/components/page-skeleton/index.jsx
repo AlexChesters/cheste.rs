d7eb86bf186b226a3fc9b00c63099cf8fd5ca844
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-06-22 09:53:33 +0100

    make image home button

--- static/src/components/page-skeleton/index.jsx
+++ static/src/components/page-skeleton/index.jsx
@@ -7,13 +7,12 @@ const Skeleton = (props) => {
   return (
     <div>
       <header className='header'>
-        <img className='avatar' src='/assets/img/me.jpg' alt='Image of Alex Chesters' tabIndex={0} />
+        <a href='/'><img className='avatar' src='/assets/img/me.jpg' alt='Image of Alex Chesters' tabIndex={0} /></a>
         <a href='/assets/alex-chesters-cv.pdf' className='header__item header__item--right-aligned blue' tabIndex={0}>CV</a>
         <a href='mailto:alex@cheste.rs' className='header__item header__item--right-aligned blue' tabIndex={0}>Email</a>
         <a href='/blog' className='header__item header__item--right-aligned blue' tabIndex={0}>Blog</a>
         <a href='/talks' className='header__item header__item--right-aligned blue' tabIndex={0}>Talks</a>
         <a href='/projects' className='header__item header__item--right-aligned blue' tabIndex={0}>Projects</a>
-        <a href='/' className='header__item header__item--right-aligned blue' tabIndex={0}>Home</a>
       </header>
       <main className='main-content'>
         {props.children}

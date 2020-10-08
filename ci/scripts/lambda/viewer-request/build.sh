ca27af04b3c3e049eb9167f808ed0f27a65f43c2
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-10-08 11:53:44 +0100

    actually fix paths

--- ci/scripts/lambda/viewer-request/build.sh
+++ ci/scripts/lambda/viewer-request/build.sh
@@ -6,9 +6,9 @@ mkdir -p build/viewer-request
 
 cd build/viewer-request
 
-cp ../../lambda/viewer-request/package.json .
-cp ../../lambda/viewer-request/package-lock.json .
+cp ../../../../lambda/viewer-request/package.json .
+cp ../../../../lambda/viewer-request/package-lock.json .
 
 npm ci --only=prod
 
-cp -R ../../lambda/viewer-request/src .
+cp -R ../../../../lambda/viewer-request/src .

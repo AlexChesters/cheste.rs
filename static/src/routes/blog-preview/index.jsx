5310b6811f0d1039765da36bfb2db36657b74e39
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-02-12 14:20:02 +0000

    add preview

--- static/src/routes/blog-preview/index.jsx
+++ static/src/routes/blog-preview/index.jsx
@@ -1,44 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import PageSkeleton from '../../components/page-skeleton'
 
 import './index.scss'
 
 export default function BlogPreview () {
-  const blogData = [
-    {
-      identifier: '008',
-      title: 'AWS Re:Invent 2019 - An Overview'
-    },
-    {
-      identifier: '007',
-      title: 'How my website is built'
-    },
-    {
-      identifier: '006',
-      title: 'AWS Re:Invent - Top Tips'
-    },
-    {
-      identifier: '005',
-      title: 'AWS Re:Invent 2018 - An Overview'
-    },
-    {
-      identifier: '004',
-      title: 'Creating your own web server in Node.js'
-    },
-    {
-      identifier: '003',
-      title: 'Creating an AWS Budget with alarms'
-    },
-    {
-      identifier: '002',
-      title: 'Hack Week December 2017 - Who\'s This?'
-    },
-    {
-      identifier: '001',
-      title: 'Hack Week May 2017 - AFK'
-    }
-  ]
+  const [blogData, setBlogData] = useState([])
+
+  async function fetchData () {
+    const data = [
+      {
+        identifier: '008',
+        title: 'AWS Re:Invent 2019 - An Overview',
+        preview: ''
+      },
+      {
+        identifier: '007',
+        title: 'How my website is built',
+        preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non ex erat. In elit nibh, hendrerit ac dui quis, faucibus.'
+      },
+      {
+        identifier: '006',
+        title: 'AWS Re:Invent - Top Tips'
+      },
+      {
+        identifier: '005',
+        title: 'AWS Re:Invent 2018 - An Overview'
+      },
+      {
+        identifier: '004',
+        title: 'Creating your own web server in Node.js'
+      },
+      {
+        identifier: '003',
+        title: 'Creating an AWS Budget with alarms'
+      },
+      {
+        identifier: '002',
+        title: 'Hack Week December 2017 - Who\'s This?'
+      },
+      {
+        identifier: '001',
+        title: 'Hack Week May 2017 - AFK'
+      }
+    ]
+
+    setBlogData(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <PageSkeleton>
@@ -52,7 +64,8 @@ export default function BlogPreview () {
                   <tr key={index} className='post__table-item'>
                     <td>
                       <a href={`/blog/${entry.identifier}`} className='post__table-item--title' tabIndex={0}>
-                        <h1 key={index}>{entry.title}</h1>
+                        <h1>{entry.title}</h1>
+                        <p>{entry.preview}</p>
                       </a>
                     </td>
                   </tr>

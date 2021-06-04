38191cd73f00bba7d0f92ea54d3fc130a696c568
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-06-04 16:02:57 +0100

    fix warning

--- static/webpack.dev.config.js
+++ static/webpack.dev.config.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/build'),
     publicPath: '/',
-    filename: 'bundle.[hash].js'
+    filename: 'bundle.[contenthash].js'
   },
   devServer: {
     contentBase: './build',
